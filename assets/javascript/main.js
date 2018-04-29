36def8b1771c88c464546f5c92b4f18da13b3cda
Thomas Gardiner <thomgardiner@gmail.com>
2018-04-29 13:32:40 -0600

    implemented score display and randomized order

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,16 +4,29 @@ const questionBank = [
  answer: 1
 },
 {question: "What is the capital of Japan?",
- options: [{id: 1, text: "Tokyo"}, {id: 2, text: "Kyoto"}, {id: 3, text: "China"}, {id: 4, text: "Seoul"}],
- answer: 1
+ options: [{id: 1, text: "Kyoto"}, {id: 2, text: "Tokyo"}, {id: 3, text: "China"}, {id: 4, text: "Seoul"}],
+ answer: 2
 },
+{question: "What is the capital of England?",
+ options: [{id: 1, text: "Paris"}, {id: 2, text: "Los Angeles"}, {id: 3, text: "London"}, {id: 4, text: "Tokyo"}],
+ answer: 3
+},
+{question: "What is the capital of France?",
+ options: [{id: 1, text: "Paris"}, {id: 2, text: "Los Angeles"}, {id: 3, text: "London"}, {id: 4, text: "Tokyo"}],
+ answer: 1
+}
 ];
 
 let score = 0;
 let currentQuestion = 0;
-let currentQ = null;
-let answer = null;
-let remainingQuestions = questionBank.length;
+
+//shuffle function
+const shuffle = function(array){
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 const gameOver = function(){
     console.log("game over");
@@ -25,6 +38,9 @@ const chooseAnswer = function(){
 
 
 const generateQuestion = function(n){
+    let remainingQuestions = questionBank.length - currentQuestion;
+    
+    $('#questions-left').html(remainingQuestions);
     if(currentQuestion < questionBank.length){
         $('#question').html('');
         $('#question').html(questionBank[n].question);
@@ -32,6 +48,7 @@ const generateQuestion = function(n){
         for(i=1; i < questionBank[n].options.length+1; i++){
             $('#button'+ i).html(questionBank[n].options[i-1].text);
         }
+       
     }
     else{
         gameOver();
@@ -41,37 +58,42 @@ const generateQuestion = function(n){
 }
 
 const checkAnswer = function(n){
-    return console.log("you selected answer " + n);
+    if(n == questionBank[currentQuestion].answer){
+        score++;
+        console.log("correct");
+        $('#score').html(score);
+    }
+    else{
+        console.log("wrong");
+    }
 }
 
 $(document).ready(function() {
 
+shuffle(questionBank);
 generateQuestion(currentQuestion);
 
 $('body').on("click", ".answer-button", function(){
     if(this.id == "button1"){
-       console.log("hey-1");
-       checkAnswer(1);
-       currentQuestion++;
-       generateQuestion(currentQuestion);
+        checkAnswer(1);
+        currentQuestion++;
+        generateQuestion(currentQuestion);
+
     }
     else if(this.id == "button2"){
-       console.log("hey-1");
-       checkAnswer(2);
-       currentQuestion++;
-       generateQuestion(currentQuestion);
+        checkAnswer(2);
+        currentQuestion++;
+        generateQuestion(currentQuestion);
     }
     else if(this.id == "button3"){
-       console.log("hey-1");
-       checkAnswer(3);
-       currentQuestion++;
-       generateQuestion(currentQuestion);
+        checkAnswer(3);
+        currentQuestion++;
+        generateQuestion(currentQuestion);
     }
     else if(this.id == "button4"){
-       console.log("hey-1");
-       checkAnswer(4);
-       currentQuestion++;
-       generateQuestion(currentQuestion);
+        checkAnswer(4);
+        currentQuestion++;
+        generateQuestion(currentQuestion);
     }
 
 
