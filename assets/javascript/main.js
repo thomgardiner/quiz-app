80cf334b452a9ed9a9894e1c6bf0e4b6e8dc7e85
Thomas Gardiner <thomgardiner@gmail.com>
2018-04-29 21:47:46 -0600

    changed up questions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,54 +1,44 @@
 //questions
 const questionBank = [
-{question: "What is the capital of Colorado?",
- options: [{id: 1, text: "Denver"}, {id: 2, text: "Boulder"}, {id: 3, text: "Texas"}, {id: 4, text: "Longmont"}],
- answer: 1,
- image: "colorado.jpeg"
+    {question: "What is the capital of Korea?",
+    options: [{id: 1, text: "Denver"}, {id: 2, text: "Seoul"}, {id: 3, text: "Texas"}, {id: 4, text: "Longmont"}],
+    answer: 2,
 },
-{question: "What is the capital of Japan?",
- options: [{id: 1, text: "Kyoto"}, {id: 2, text: "Tokyo"}, {id: 3, text: "China"}, {id: 4, text: "Seoul"}],
- answer: 2,
- image: "japan.jpeg"
+    {question: "What is the capital of Japan?",
+    options: [{id: 1, text: "Kyoto"}, {id: 2, text: "Tokyo"}, {id: 3, text: "China"}, {id: 4, text: "Seoul"}],
+    answer: 2,
 },
-{question: "What is the capital of England?",
- options: [{id: 1, text: "Paris"}, {id: 2, text: "Los Angeles"}, {id: 3, text: "London"}, {id: 4, text: "Tokyo"}],
- answer: 3,
- image: "england.jpeg"
+    {question: "What is the capital of England?",
+    options: [{id: 1, text: "Paris"}, {id: 2, text: "Los Angeles"}, {id: 3, text: "London"}, {id: 4, text: "Tokyo"}],
+    answer: 3,
+    },
+    {question: "What is the capital of France?",
+    options: [{id: 1, text: "Tokyo"}, {id: 2, text: "Paris"}, {id: 3, text: "London"}, {id: 4, text: "Los Angeles"}],
+    answer: 2,
 },
-{question: "What is the capital of France?",
- options: [{id: 1, text: "Tokyo"}, {id: 2, text: "Paris"}, {id: 3, text: "London"}, {id: 4, text: "Los Angeles"}],
- answer: 2,
- image: "france.jpeg"
+    {question: "What is the capital of Italy?",
+    options: [{id: 1, text: "Beijing"}, {id: 2, text: "Rome"}, {id: 3, text: "Mexico City"}, {id: 4, text: "Tokyo"}],
+    answer: 2,
 },
-{question: "What is the capital of Italy?",
- options: [{id: 1, text: "Beijing"}, {id: 2, text: "Rome"}, {id: 3, text: "Mexico City"}, {id: 4, text: "Tokyo"}],
- answer: 2,
- image: "italy.jpeg"
+    {question: "What is the capital of Russia?",
+    options: [{id: 1, text: "Lisbon"}, {id: 2, text: "Rome"}, {id: 3, text: "Moscow"}, {id: 4, text: "New York City"}],
+    answer: 3,
 },
-{question: "What is the capital of Russia?",
- options: [{id: 1, text: "Beijing"}, {id: 2, text: "Rome"}, {id: 3, text: "Moscow"}, {id: 4, text: "New York City"}],
- answer: 3,
- image: "russia.jpeg"
+    {question: "What is the capital of Mexico?",
+    options: [{id: 1, text: "New York City"}, {id: 2, text: "Rome"}, {id: 3, text: "Moscow"}, {id: 4, text: "Mexico City"}],
+    answer: 4,
 },
-{question: "What is the capital of Mexico?",
- options: [{id: 1, text: "New York City"}, {id: 2, text: "Rome"}, {id: 3, text: "Moscow"}, {id: 4, text: "Mexico City"}],
- answer: 4,
- image: "mexico.jpeg"
+    {question: "What is the capital of Canada?",
+    options: [{id: 1, text: "London"}, {id: 2, text: "Ottawa"}, {id: 3, text: "Paris"}, {id: 4, text: "Seoul"}],
+    answer: 2,
 },
-{question: "What is the capital of Canada?",
- options: [{id: 1, text: "London"}, {id: 2, text: "Ottawa"}, {id: 3, text: "Paris"}, {id: 4, text: "Seoul"}],
- answer: 2,
- image: "canada.jpeg"
+    {question: "What is the capital of China?",
+    options: [{id: 1, text: "Beijing"}, {id: 2, text: "Tokyo"}, {id: 3, text: "Lisbon"}, {id: 4, text: "Moscow"}],
+    answer: 1,
 },
-{question: "What is the capital of China?",
- options: [{id: 1, text: "Moscow"}, {id: 2, text: "Tokyo"}, {id: 3, text: "Lisbon"}, {id: 4, text: "Beijing"}],
- answer: 4,
- image: "china.jpeg"
-},
-{question: "What is the capital of Portugal?",
- options: [{id: 1, text: "Moscow"}, {id: 2, text: "Tokyo"}, {id: 3, text: "Lisbon"}, {id: 4, text: "Beijing"}],
- answer: 4,
- image: "portugal.jpeg"
+    {question: "What is the capital of Portugal?",
+    options: [{id: 1, text: "Moscow"}, {id: 2, text: "Tokyo"}, {id: 3, text: "Lisbon"}, {id: 4, text: "Beijing"}],
+    answer: 3,
 }
 ];
 
@@ -70,7 +60,6 @@ const shuffle = function(array){
 const gameOver = function(){
     $('.answer-button').hide();
     $('#question').html("Your score was " + score + " out of " + possibleScore + " (" + (score / possibleScore * 100) + "%)");
-    
     let gameOver = $("<div>");
     gameOver.attr("id", "game-over-message");
     gameOver.html("Click above or press a key to play again!");
@@ -82,7 +71,6 @@ const gameOver = function(){
 //generates questions from the question bank
 const generateQuestion = function(n){
     let remainingQuestions = questionBank.length - currentQuestion;
-    
     $('#questions-left').html(remainingQuestions);
     if(currentQuestion < questionBank.length){
         $('#question').html('');
@@ -91,7 +79,6 @@ const generateQuestion = function(n){
         for(i=1; i < questionBank[n].options.length+1; i++){
             $('#button'+ i).html(questionBank[n].options[i-1].text);
         }
-       
     }
     else{
         gameOver();
@@ -181,6 +168,8 @@ $('body').on("click", ".answer-button", function(){
     }
 })
 
+//hover logic
+
 $('.answer-button').hover(function(){
     let id = this.id;
     $('#' + id).addClass("selected");
@@ -190,10 +179,10 @@ $('.answer-button').hover(function(){
 });
 
 
-//resets the quiz if game is over 
+//reset logic
 
 $('body').on("click", "#question-area", function(){
-    if(running === false){
+    if(running == false){
         reset();
     }
 })
